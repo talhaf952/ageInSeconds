e62391d617c535ea68bfc137fca37a9ff2840693
Muhammad Talha <127777461+talhaf952@users.noreply.github.com>
2023-08-24 08:49:41 -0800

    initial

---
+++ ageInSeconds.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter Your age in year: ";
+	cin>>age;
+	totalAge=age*365*24*60*60;
+	//365 days, 24 hours, 6o minutes, 60 seconds.
+}
